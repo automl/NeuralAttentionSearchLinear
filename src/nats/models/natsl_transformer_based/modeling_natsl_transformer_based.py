00a7734d00b819324c28953b779903a96117c508
dengdifan <difandeng@gmail.com>
2025-09-15 23:56:12 +0200

    maint

--- src/nats/models/natsl_transformer_based/modeling_natsl_transformer_based.py
+++ src/nats/models/natsl_transformer_based/modeling_natsl_transformer_based.py
@@ -281,6 +281,8 @@ class NeuralAttentionSearchLinearTransformerBasedModel(NeuralAttentionSearchLine
 
         all_hidden_states = () if output_hidden_states else None
         all_attns = () if output_attentions else None
+        next_cache = None
+
         for layer in self.layers:
             if output_hidden_states:
                 all_hidden_states += (hidden_states,)
